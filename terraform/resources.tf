4f226255f3a53a82591602d7caf9961776f691e7
Kevin Printz <kevinprintz@microsoft.com>
2023-03-26 22:35:03 -0400

    fix

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -12,12 +12,13 @@ module "vnet" {
 }
 
 module "vm" {
-  source          = "../../az-pipeline-template-tf-ansible/terraform/vm"
-  location        = azurerm_resource_group.rg.location
-  rg_name         = azurerm_resource_group.rg.name
-  kv_rg_name      = "rg-keyvault"
-  kv_name         = "kv-mgmt-coffeetimedev"
-  kv_secret_name  = "vm-secret"
-  subnet_id       = module.vnet.subnet_id
-  vm_name         = "vm-terraform-ansible"
+  source           = "../../az-pipeline-template-tf-ansible/terraform/vm"
+  location         = azurerm_resource_group.rg.location
+  rg_name          = azurerm_resource_group.rg.name
+  kv_rg_name       = "rg-keyvault"
+  kv_name          = "kv-mgmt-coffeetimedev"
+  kv_secret_name   = "vm-secret"
+  subnet_id        = module.vnet.subnet_id
+  vm_name          = "vm-terraform-ansible"
+  ansible_playbook = "ansible/vm1/playbook.yml"
 }
